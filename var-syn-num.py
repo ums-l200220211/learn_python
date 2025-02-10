b72abc12f6b833c3ba24cf924113fcac5af748ad
NAUFALZULHM <naufalmaulanasans@gmail.com>
2025-02-10 20:07:50 +0700

    learn basic

---
+++ var-syn-num.py
@@ -0,0 +1,28 @@
+nama = 'Naufal'
+nim = 'l200220211'
+prodi = 'teknik informatika'
+
+mhs = "ada seorang mahasigma yang bernama " + nama + " dengan NIM " + nim + " yang sedang menempuh pendidikan di prodi " + prodi
+print(mhs)
+
+nama = 'Maulana '
+print(nama*3)
+
+dzikir = 'subhanalah Walhamdulillah wala ilAha illallah wallahuakbar'
+print(dzikir.count('la'))
+
+print(dzikir.replace('la', 'LA'))
+
+print(dzikir.upper())
+
+print(dzikir.lower())
+
+print(dzikir.capitalize())
+
+print(dzikir.title())
+
+print(dzikir.swapcase()) #kebalikannya
+
+
+
+
