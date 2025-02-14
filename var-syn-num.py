d530fa74d3ab87f4c2f1564615af8ec4b9fcf0e3
NAUFALZULHM <naufalmaulanasans@gmail.com>
2025-02-14 20:05:57 +0700

    learn var py

--- var-syn-num.py
+++ var-syn-num.py
@@ -40,10 +40,96 @@ if 5 > 2 :
     print("Five is greater than two!") #menjorok
 if 5 > 2 :
         print("Five is greater than two!") # lebih menjorok
-               print("Five is greater than two!") #error
+            #    print("Five is greater than two!") #error
                
 # Variabel
 x = 2
+x = "sally"
+X = 3 # X and x are different variables
 y = "Hello, World!"
 print(x)
 print(y)
+
+# Comments(this is a comment)
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+# variable casting(specify a variable data type)
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+print(f"x = {x}, y = {y}, z = {z}") # f-string
+print(type(x), type(y), type(z)) 
+
+"""Variable Names
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+"""
+
+# Legal Variabel Name
+myvar = "zul"
+my_var = "zul"
+_my_var = "zul"
+myVar = "zul"
+MYVAR = "zul"
+myvar2 = "zul"
+
+print(myvar, my_var, _my_var, myVar, MYVAR, myvar2)
+
+# Illegal Variabel Name
+# 2myvar = "zul" #number
+# my-var = "zul" #minus
+# my var = "zul" #space
+
+# Camel Case
+myVariableName = "Zul"
+# Pascal Case
+MyVariableName = "Zul"
+# Snake Case
+my_variable_name = "Zul"
+print(myVariableName, MyVariableName, my_variable_name)
+
+# Many Values to Multiple Variables
+buah, warna, harga = "jeruk", "Orange", 2000
+print(buah, warna, harga)
+
+# One Value to Multiple Variables
+buah = sayur = tanaman = "Tomat"
+print(buah, sayur, tanaman)
+
+# Unpack a List
+workOut = ["push up", "pull up", "sit up"]
+Chestday, Backday, Absday = workOut
+print("list = ",Chestday, Backday, Absday)
+# Unpack a Tuple
+WorkOut = ("Push up", "Pull up", "Sit up")
+Chestday, Backday, Absday = WorkOut
+print("tuple = ",Chestday, Backday, Absday)
+# Unpack a String
+WorkOut = "Push up, Pull up, Sit up"
+(Chestday, Backday, Absday) = WorkOut.split(", ") #split string
+print("string = ",Chestday, Backday, Absday)
+
+# Global Variables and Local Variables
+barbel = "20kg"
+def bebanBarbel():
+    Barbel = "10kg" #local variable
+    
+    global barbel
+    barbel = "30kg" #global variable (change the value of a global variable inside a function)
+    
+    print(f"I am doing chest day with {Barbel} and {barbel}" ) 
+bebanBarbel()
+print(f"I am doing chest day with {barbel}") #global variable
+
+x = 4 
+cmplx = complex(x) #complex number
+print(type(cmplx)) 
+print(len(cmplx)) #error 
