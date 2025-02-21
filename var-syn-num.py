01371446655dd414aa5d39021974cd895dca31ef
NAUFALZULHM <naufalmaulanasans@gmail.com>
2025-02-21 20:52:18 +0700

    learn if else

--- var-syn-num.py
+++ var-syn-num.py
@@ -732,6 +732,66 @@ print(myfamily)
 
 # Access Items in Nested Dictionaries
 print(myfamily["child1"]["name"]) #Emil
-print(myfamily["child2"]["year"]) #2007
+print(myfamily["child2"]["age"]) #24
 print(myfamily["child3"]["name"]) #Linus
 
+# Python Conditions and If statements logical operators
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+    
+# elif
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+    
+# Short Hand If
+if a > b: print("a is greater than b")
+
+# Short Hand If ... Else
+print("A") if a > b else print("B")
+
+# one line if else statement, with 3 conditions
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# And
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("Both conditions are True")
+
+# Or
+if a > b or a > c:
+    print("At least one of the conditions is True")
+
+# Not
+if not a > b:
+    print("a is not greater than b")
+
+# Nested If
+x = 41
+if x > 10:
+    print("Above ten,")
+    if x > 20:
+        print("and also above 20!")
+    else:
+        print("but not above 20.")
+
+# The pass Statement
+a = 33
+b = 200
+if b > a:
+    pass #empty block of code that does nothing 
+    
+    
